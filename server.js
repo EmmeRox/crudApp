1f4e3a6a538066be56a28b3b29016b607fb17718
EmmeRox <99152085+EmmeRox@users.noreply.github.com>
2023-09-02 18:36:39 -0500

    moved all handlers inside the connect method
    rearranged all to be inside to have access to the db variable and the db collection

--- server.js
+++ server.js
@@ -6,8 +6,6 @@ const MongoClient = require("mongodb").MongoClient;
 const url =
   "mongodb+srv://emmeocampo:RC0jmoo19apzRu1y@cluster0.ofk6hxs.mongodb.net/?retryWrites=true&w=majority";
 
-//Make sure to have the body-parser before the CRUD handlers
-app.use(bodyParser.urlencoded({ extended: true }));
 //The urlencoded method tells the body-parser to extract date from the form and add them to the body property in the request object. Now you can see the values from the form element inside req.body now
 
 // using callbacks below
@@ -20,17 +18,24 @@ app.use(bodyParser.urlencoded({ extended: true }));
 MongoClient.connect(url)
   .then((client) => {
     console.log("Connected to Database");
+    const db = client.db("quote-app");
+    const quotesCollection = db.collection("quotes");
+    //express request handlers go here bc this is where the db variable is and it is necessary
+    //Make sure to have the body-parser before the CRUD handlers
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.get("/", (req, res) => {
+      res.sendFile("/Users/timmy/crudApp" + "/index.html");
+    });
+    app.post("/quotes", (req, res) => {
+      quotesCollection
+        .insertOne(req.body)
+        .then((result) => {
+          res.redirect("/");
+        })
+        .catch((error) => console.log(error));
+    });
+    app.listen(3000, function () {
+      console.log("listening on 3000");
+    });
   })
   .catch((error) => console.log(error));
-
-app.listen(3000, function () {
-  console.log("listening on 3000");
-});
-
-app.get("/", (req, res) => {
-  res.sendFile("/Users/timmy/crudApp" + "/index.html");
-});
-
-app.post("/quotes", (req, res) => {
-  console.log(req.body);
-});
